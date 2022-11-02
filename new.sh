326e1ccb9fc75ff49f9ed0e7f92f7e043f6f33bf
Varun Sonavni <varunsonavne@gmail.com>
2022-11-02 06:58:44 +0000

    Update .gitlab-ci.yml, new.sh

--- new.sh
+++ new.sh
@@ -1,9 +1,10 @@
-PROJECTKEY="."
-QGSTATUS='curl -s -u  SONAR_TOKEN: http://SONAR_URL:9000/api/qualitygates/project_status?projectKey=$PROJECTKEY | jq '.projectStatus.status' | tr -d '"''
-if [ "$QGSTATUS" = "OK" ]
-then
-exit 0
-elif [ "$QGSTATUS" = "ERROR" ]
-then
-exit 1
+export QGSTATUS=`curl -s -u  $SONAR_TOKEN: http://18.232.146.98:9000/api/qualitygates/project_status?projectKey=new-group127_static-website-007_AYQy5EeRxITt09_C-Nlc | jq '.projectStatus.status' | tr -d '"'`
+if [ "$QGSTATUS" = "ERROR" ]
+  then
+  echo "exit"
+  exit 1
+elif [ "$QGSTATUS" = "OK" ]
+  then
+  echo "ok"
+  exit 0
 fi
